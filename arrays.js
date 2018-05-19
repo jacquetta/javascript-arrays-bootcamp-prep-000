c62c242ce9caaa5dcff409abb4da25b1fde21459
Jacquetta Bommuluri <jacquettavb@hotmail.com>
2018-05-19 07:12:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,11 +13,11 @@ function addElementToBeginningOfArray(a, b){
 }
 addElementToBeginningOfArray();
 
-function desructiveAddElementToBeginningOfArray(a, b){
+function destructivelyAddElementToBeginningOfArray(a, b){
   return a = a.unshift(b);
 
 }
-descructiveAddElementToBeginningOfArray();
+destructivelyAddElementToBeginningOfArray();
 
 function addElementToEndOfArray(c, d) {
   return [...c, d];
@@ -47,33 +47,3 @@ function removeElementFromEndOfArray(j) {
 }
 
 removeElementFromEndOfArray();
-// var meArray = [];
-// var brandArray;
-// function addElementToBeginningOfArray(a, b){
-//   brandArray = a.unshift(b);;
-//   return brandArray;
-//
-//
-// }
-// addElementToBeginningOfArray(meArray,);
-//
-// var nextArray =[];
-// function destructivelyAddElementToBeginningOfArray(a, b){
-//     return a.unshift(b);
-//
-// }
-// destructivelyAddElementToBeginningOfArray(nextArray, );
-//
-// var getArray = [];
-// var updateArray;
-// function addElementToEndOfArray(a, b){
-//   updateArray = a.push(b);
-//   return updateArray;
-// }
-// addElementToEndOfArray(getArray,);
-//
-// var lastArray = [];
-// function destructivelyAddElementToEndOfArray(){
-//   return a.push(b);
-// }
-// destructivelyAddElementToEndOfArray(lastArray,);
