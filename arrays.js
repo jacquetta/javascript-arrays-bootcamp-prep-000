00177fe362d715bbb4fa85a307103d0d85ebdb16
Jacquetta Bommuluri <jacquettavb@hotmail.com>
2018-05-19 07:09:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,33 +8,72 @@ var cat = [];
 accessElementInArray(cat);
 
 
-var meArray = [];
-var brandArray;
 function addElementToBeginningOfArray(a, b){
-  brandArray = a.unshift(b);;
-  return brandArray;
+  return [b, ...a];
+}
+addElementToBeginningOfArray();
 
+function desructiveAddElementToBeginningOfArray(a, b){
+  return a = a.unshift(b);
 
 }
-addElementToBeginningOfArray(meArray,);
+descructiveAddElementToBeginningOfArray();
 
-var nextArray =[];
-function destructivelyAddElementToBeginningOfArray(a, b){
-    return a.unshift(b);
-
+function addElementToEndOfArray(c, d) {
+  return [...c, d];
 }
-destructivelyAddElementToBeginningOfArray(nextArray, );
+addElementToEndOfArray();
 
-var getArray = [];
-var updateArray;
-function addElementToEndOfArray(a, b){
-  updateArray = a.push(b);
-  return updateArray;
+function destructivelyAddElementToEndOfArray(c, d){
+  return c = c.push(d);
 }
-addElementToEndOfArray(getArray,);
 
-var lastArray = [];
-function destructivelyAddElementToEndOfArray(){
-  return a.push(b);
+destructivelyAddElementToEndOfArray();
+
+
+function destructivelyRemoveElementFromBeginningOfArray(a){
+  return a.shift();
 }
-destructivelyAddElementToEndOfArray(lastArray,);
+
+destructivelyRemoveElementFromBeginningOfArray();
+
+function destructivelyRemoveElementFromEndOfArray(b){
+  return b.pop();
+}
+destructivelyRemoveElementFromEndOfArray();
+
+function removeElementFromEndOfArray(j) {
+  return j.slice(0, j.length - 1);
+}
+
+removeElementFromEndOfArray();
+// var meArray = [];
+// var brandArray;
+// function addElementToBeginningOfArray(a, b){
+//   brandArray = a.unshift(b);;
+//   return brandArray;
+// 
+// 
+// }
+// addElementToBeginningOfArray(meArray,);
+// 
+// var nextArray =[];
+// function destructivelyAddElementToBeginningOfArray(a, b){
+//     return a.unshift(b);
+// 
+// }
+// destructivelyAddElementToBeginningOfArray(nextArray, );
+// 
+// var getArray = [];
+// var updateArray;
+// function addElementToEndOfArray(a, b){
+//   updateArray = a.push(b);
+//   return updateArray;
+// }
+// addElementToEndOfArray(getArray,);
+// 
+// var lastArray = [];
+// function destructivelyAddElementToEndOfArray(){
+//   return a.push(b);
+// }
+// destructivelyAddElementToEndOfArray(lastArray,);
