c88212d6a6945622c3b0c7dcd9c4d411fd417aa0
Jacquetta Bommuluri <jacquettavb@hotmail.com>
2018-05-19 06:12:43 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,41 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+
+var meArray = [];
+var brandArray;
+function addElementToBeginningOfArray(a, b){
+  brandArray = a.unshift(b);;
+  return brandArray;
+
+
+}
+addElementToBeginningOfArray(meArray,);
+
+var nextArray =[];
+function destructivelyAddElementToBeginningOfArray(a, b){
+    return a.unshift(b);
+
+}
+destructivelyAddElementToBeginningOfArray(nextArray, );
+
+var getArray = [];
+var updateArray;
+function addElementToEndOfArray(a, b){
+  updateArray = a.push(b);
+  return updateArray;
+}
+addElementToEndOfArray(getArray,);
+
+var lastArray = [];
+function destructivelyAddElementToEndOfArray(){
+  return a.push(b);
+}
+destructivelyAddElementToEndOfArray(lastArray,);
+
+
+function accessElementInArray(c){
+  return c[3];
+}
+
+var cat = [];
+accessElementInArray(cat);
